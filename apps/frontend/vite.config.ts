43ba513f64cb2af64c5135ea74df9ff68e743afd
Hamza <hamzaravani0@gmail.com>
2025-12-22 18:06:22 +0530

    pwa: basic configuration using vite-plugin-pwa

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -3,6 +3,7 @@ import { sentryVitePlugin } from "@sentry/vite-plugin";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig, loadEnv } from "vite";
 import { analyzer } from 'vite-bundle-analyzer';
+import { VitePWA } from "vite-plugin-pwa";
 import { envValidationVitePlugin } from "./src/utils/env_validation_vite_plugin.utils";
 
 // https://vite.dev/config/
@@ -21,6 +22,45 @@ export default defineConfig(({ mode }) => {
 			analyzerMode: "static",
 			fileName: ".dev/stats.html",
 			openAnalyzer: true,
+		}),
+		VitePWA({
+			registerType: "autoUpdate",
+			includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+			devOptions: {
+				enabled: process.env.NODE_ENV === "development"
+				},
+			manifest: {
+				name: "OneQ",
+				short_name: "OneQ",
+				start_url: "/",
+				display: "standalone",
+				background_color: "#ffffff",
+				theme_color: "#1976d2",
+				icons: [
+					{
+						src: 'pwa-64x64.png',
+						sizes: '64x64',
+						type: 'image/png'
+					  },
+					  {
+						src: 'pwa-192x192.png',
+						sizes: '192x192',
+						type: 'image/png'
+					  },
+					  {
+						src: 'pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'any'
+					  },
+					  {
+						src: 'maskable-icon-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'maskable'
+					  }
+				]
+			}
 		}),
 		// Put the Sentry vite plugin after all other plugins
     sentryVitePlugin({
